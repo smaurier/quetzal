2b1ffec1d1663f81e9c6939d95519cac34fb8723
Sylvain <sylvain.maurier@gmail.com>
2025-03-19 20:04:32 +0100

    retry

--- quetzal-backend/src/main.ts
+++ quetzal-backend/src/main.ts
@@ -5,11 +5,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://quetzal-theta.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3001);
+  // Render fournit la variable PORT, utilisons-la avec une valeur par défaut sécurisée
+  const port = parseInt(process.env.PORT || '3001');
+  console.log(`Application starting on port: ${port}`);
+
+  await app.listen(port, '0.0.0.0'); // Ajout de '0.0.0.0' pour écouter sur toutes les interfaces
 }
 bootstrap();
