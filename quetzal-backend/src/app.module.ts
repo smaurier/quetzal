c45ec771ca5fa865bc0f5af9b3b492e24decd86c
Sylvain <sylvain.maurier@gmail.com>
2025-03-19 18:41:26 +0100

    fix front to set mongo

--- quetzal-backend/src/app.module.ts
+++ quetzal-backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsService } from './items/items.service';
 import { ItemsController } from './items/items.controller';
@@ -6,7 +7,20 @@ import { Item, ItemSchema } from './schemas/item-test.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    // Chargement des variables d'environnement
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Configuration de Mongoose avec l'URI de MongoDB
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+
     MongooseModule.forFeature([{ name: Item.name, schema: ItemSchema }]),
   ],
   controllers: [ItemsController],
