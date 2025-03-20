53b95f0e66c6e32c01d09698ed521fda2adc21db
Sylvain <sylvain.maurier@gmail.com>
2025-03-20 13:33:30 +0100

    cors resolve

--- quetzal-backend/src/main.ts
+++ quetzal-backend/src/main.ts
@@ -4,16 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Appliquer CORS pour toutes les requêtes venant de Vercel et localhost
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://quetzal-theta.vercel.app/'],
+    origin: ['https://quetzal-theta.vercel.app', 'http://localhost:3000'], // Listes des origines autorisées
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    credentials: true, // Permet l'accès aux cookies et autres informations de session
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Ajout des en-têtes nécessaires
   });
 
-  // Render fournit la variable PORT, utilisons-la avec une valeur par défaut sécurisée
   const port = parseInt(process.env.PORT || '3001');
   console.log(`Application starting on port: ${port}`);
 
-  await app.listen(port, '0.0.0.0'); // Ajout de '0.0.0.0' pour écouter sur toutes les interfaces
+  await app.listen(port, '0.0.0.0'); // Assure-toi que l'application écoute sur toutes les interfaces
 }
 bootstrap();
